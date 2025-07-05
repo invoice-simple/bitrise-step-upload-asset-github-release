2456ca17903af5150416b27e4f141c8123708aa0
Gabs Simonetti <gabs@invoicesimple.com>
2025-07-04 19:58:39 -0700

    fix(release): sed fix

--- update_version_files.sh
+++ update_version_files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 new_version="$1"
-file_path="step.yml"
 
-sed -i '' "s/^version: .*/version: ${new_version}/" "${file_path}"
+sed -i '' "s/^version: .*/version: ${new_version}/" step.yml
 
-echo "Updated version to ${new_version} in ${file_path}"
+echo "Updated version to ${new_version} in step.yml
